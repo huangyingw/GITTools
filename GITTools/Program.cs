825c5e61cfa9ce01af0a2b0f3abcd42e462d110b
huangyingw@huangyingw-01.mscnc <huangyingw@gmail.com>
2010-03-25 10:58:04 +0800

    no

--- GITTools/Program.cs
+++ GITTools/Program.cs
@@ -10,12 +10,15 @@ namespace GITTools
     {
         static void Main(string[] args)
         {
-            string source = @"E:\temp\source";
+            string source = @"E:\temp\source\tools";
             string target = @"E:\temp\target";
             DirectoryInfo sdi = new DirectoryInfo(source);
             foreach (FileSystemInfo item in sdi.GetFileSystemInfos())
             {
-                
+                if (Directory.Exists(item.FullName))
+                {   
+                    
+                }   
             }
         }
     }
