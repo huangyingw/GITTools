eb2680061e5faab8ed3fc54e61822413fb1951b6
huangyingw@huangyingw-01.mscnc <huangyingw@gmail.com>
2010-03-25 11:16:11 +0800

    no

--- GITTools/Program.cs
+++ GITTools/Program.cs
@@ -21,22 +21,18 @@ namespace GITTools
                     if (!Directory.Exists(item.FullName+@"\.git"))
                     {
                         Console.WriteLine(item.FullName);
-                        //string command = @" item.FullName";
-                        //System.Diagnostics.Process.Start("cd", command);
-                        //command = @" in";
-                        //System.Diagnostics.Process.Start("git", command);
                         Process p = new Process();
                         p.StartInfo.FileName = "cmd.exe";
-                        // 这里是关键点,不用Shell启动/重定向输入/重定向输出/不显示窗口
                         p.StartInfo.UseShellExecute = false;
                         p.StartInfo.RedirectStandardInput = true;
                         p.StartInfo.RedirectStandardOutput = true;
                         p.StartInfo.CreateNoWindow = true;
                         p.Start();
-                        p.StandardInput.WriteLine("ping 127.0.0.1");// 向cmd.exe输入command
+                        p.StandardInput.WriteLine("cd "+item.FullName);
+                        p.StandardInput.WriteLine("git in");
                         p.StandardInput.WriteLine("exit");
                         p.WaitForExit(60000);
-                        string s = p.StandardOutput.ReadToEnd();// 得到cmd.exe的输出
+                        string s = p.StandardOutput.ReadToEnd();
                         p.Close();
                         Console.WriteLine(s);
 
