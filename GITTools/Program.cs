367258cf2507862629737af752cd5332363b9e1b
huangyingw@vsts 2008 <huangyingw@gmail.com>
2010-03-25 10:52:48 -0400

    no

--- GITTools/Program.cs
+++ GITTools/Program.cs
@@ -14,31 +14,38 @@ namespace GITTools
             string source = @"C:\source";
             string target = @"E:\temp\target";
             DirectoryInfo sdi = new DirectoryInfo(source);
+            string result = null;
             foreach (FileSystemInfo item in sdi.GetFileSystemInfos())
             {
                 if (Directory.Exists(item.FullName))
                 {
                     if (!Directory.Exists(item.FullName + @"\.git"))
                     {
+                        result = null;
                         Console.WriteLine(item.FullName);
-                        Process p = new Process();
-                        p.StartInfo.FileName = "cmd.exe";
-                        p.StartInfo.UseShellExecute = false;
-                        p.StartInfo.RedirectStandardInput = true;
-                        p.StartInfo.RedirectStandardOutput = true;
-                        p.StartInfo.CreateNoWindow = true;
-                        p.Start();
-                        p.StandardInput.WriteLine("cd " + item.FullName);
-                        p.StandardInput.WriteLine("git in");
-                        p.StandardInput.WriteLine("exit");
-                        p.WaitForExit(60000);
-                        string s = p.StandardOutput.ReadToEnd();
-                        p.Close();
-                        Console.WriteLine(s);
-
+                        result += CallDos(@"cd " + item.FullName);
+                        result += CallDos("git in");
+                        result += CallDos("exit");
+                        Console.WriteLine(result);
                     }
                 }
             }
+        }
+
+        static string CallDos(string command)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            p.StandardInput.WriteLine(command);
+            p.WaitForExit();
+            string s = p.StandardOutput.ReadToEnd();
+            p.Close();
+            return s;
         }
     }
 }
