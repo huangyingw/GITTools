43250246a136bb8cd7adb90c6c5b32a6aaa932bd
huangyingw@huangyingw-01.mscnc <huangyingw@gmail.com>
2010-03-25 10:39:40 +0800

    no

--- GITTools/Program.cs
+++ GITTools/Program.cs
@@ -10,15 +10,14 @@ namespace GITTools
     {
         static void Main(string[] args)
         {
-            string source=@"E:\temp\source";
+            string source = @"E:\temp\source";
             string target = @"E:\temp\target";
-            if (File.Exists(path))
-        return new FileInfo(path);
-    else if (Directory.Exists(path))
-        return new DirectoryInfo(path);
+            FileSystemInfo fsi = null;
+            if (File.Exists(source))
+                fsi = new FileInfo(source);
+            else if (Directory.Exists(source))
+                fsi = new DirectoryInfo(source);
 
-            FileSystemInfo fsi=new 
-            
         }
     }
 }
