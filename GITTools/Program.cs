52413ffa4e99ec56b7ac9181a0fdde1d471e5535
huangyingw@huangyingw-01.mscnc <huangyingw@gmail.com>
2010-03-25 11:17:08 +0800

    n

--- GITTools/Program.cs
+++ GITTools/Program.cs
@@ -20,6 +20,20 @@ namespace GITTools
                     if (!Directory.Exists(item.FullName+@"\.git"))
                     {
                         Console.WriteLine(item.FullName);
+                        p = new Process();
+                        p.StartInfo.FileName = "cmd.exe";
+                        // 这里是关键点,不用Shell启动/重定向输入/重定向输出/不显示窗口
+                        p.StartInfo.UseShellExecute = false;
+                        p.StartInfo.RedirectStandardInput = true;
+                        p.StartInfo.RedirectStandardOutput = true;
+                        p.StartInfo.CreateNoWindow = true;
+                        p.Start();
+                        p.StandardInput.WriteLine("ping 127.0.0.1");// 向cmd.exe输入command
+                        p.StandardInput.WriteLine("exit");
+                        p.WaitForExit(60000);
+                        string s = p.StandardOutput.ReadToEnd();// 得到cmd.exe的输出
+                        p.Close();
+
                     }
                 }   
             }
