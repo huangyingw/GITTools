f20b102f745dd4e0e2844b9cdbe164edaf74565a
huangyingw@linux_programming <huangyingw@gmail.com>
2010-04-29 02:11:26 -0400

    n

---
+++ src/git.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class git {
+	private static ArrayList filelist = new ArrayList(); 
+    
+    public static void main(String[] args) {
+        
+        long a = System.currentTimeMillis();
+        refreshFileList("c:\\java");
+        System.out.println(System.currentTimeMillis() - a);
+    }
+    public static void refreshFileList(String strPath) { 
+        File dir = new File(strPath); 
+        File[] files = dir.listFiles(); 
+        
+        if (files == null) 
+            return; 
+        for (int i = 0; i < files.length; i++) { 
+            if (files[i].isDirectory()) { 
+                refreshFileList(files[i].getAbsolutePath()); 
+            } else { 
+                String strFileName = files[i].getAbsolutePath().toLowerCase();
+                System.out.println("---"+strFileName);
+                filelist.add(files[i].getAbsolutePath());                    
+            } 
+        } 
+    }
+
+}
