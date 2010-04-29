fe4fd8e9c35901b4c5ea4ac8233f82ae64215b51
huangyingw@fareast <huangyingw@gmail.com>
2010-04-29 13:37:13 +0800

    n

--- git.java
+++ git.java
@@ -6,3 +6,31 @@ public class git
     System.out.println("Hello, World!");
   }
 }
+ import java.io.File;
+import java.util.ArrayList;
+public class FileSystem1 {
+    private static ArrayList filelist = new ArrayList(); 
+    
+    public static void main(String[] args) {
+        
+        long a = System.currentTimeMillis();
+        refreshFileList("c:\\java");
+        System.out.println(System.currentTimeMillis() - a);
+    }
+    public static void refreshFileList(String strPath) { 
+        File dir = new File(strPath); 
+        File[] files = dir.listFiles(); 
+        
+        if (files == null) 
+            return; 
+        for (int i = 0; i < files.length; i++) { 
+            if (files[i].isDirectory()) { 
+                refreshFileList(files[i].getAbsolutePath()); 
+            } else { 
+                String strFileName = files[i].getAbsolutePath().toLowerCase();
+                System.out.println("---"+strFileName);
+                filelist.add(files[i].getAbsolutePath());                    
+            } 
+        } 
+    }
+}
