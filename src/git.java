663c98d999f292b609229c15c8eedd0d37ddf8fd
huangyingw@huangyingw-01.mscnc <huangyingw@gmail.com>
2010-04-29 17:03:32 +0800

    n

--- src/git.java
+++ src/git.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class git {
 	
 	private static ArrayList filelist = new ArrayList(); 
-  public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception{
   	String cmd = "ls -al";
 		Runtime run = Runtime.getRuntime();
 		Process pr = run.exec(cmd);
