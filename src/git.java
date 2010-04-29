4124cb763b1e45181558db0c53579519b500d359
huangyingw@linux_programming <huangyingw@gmail.com>
2010-04-29 02:54:59 -0400

    n

--- src/git.java
+++ src/git.java
@@ -1,8 +1,4 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
 import java.lang.* ;
-import java.util.ArrayList;
 
 public class git {
 	public static void main(String[] args) {
