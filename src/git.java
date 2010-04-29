592e94cc7654e1884dd02bbe70da97677491f218
huangyingw@fareast <huangyingw@gmail.com>
2010-04-29 14:40:24 +0800

    n

--- src/git.java
+++ src/git.java
@@ -5,11 +5,10 @@ import java.lang.* ;
 import java.util.ArrayList;
 
 public class git {
+	
 	private static ArrayList filelist = new ArrayList(); 
-    
-    public static void main(String[] args) {
-        
-    	String cmd = "ls -al";
+  public static void main(String[] args) {
+  	String cmd = "ls -al";
 		Runtime run = Runtime.getRuntime();
 		Process pr = run.exec(cmd);
 		pr.waitFor();
@@ -17,24 +16,7 @@ public class git {
 		String line = "";
 		while ((line=buf.readLine())!=null) {
 			System.out.println(line);
-		}
-    }
-    public static void refreshFileList(String strPath) { 
-        File dir = new File(strPath); 
-        File[] files = dir.listFiles(); 
-        
-        if (files == null) 
-            return; 
-        for (int i = 0; i < files.length; i++) { 
-            if (files[i].isDirectory()) { 
-                refreshFileList(files[i].getAbsolutePath()); 
-            } else { 
-                String strFileName = files[i].getAbsolutePath().toLowerCase();
-                System.out.println("---"+strFileName);
-                filelist.add(files[i].getAbsolutePath());                    
-            } 
-        } 
-    }
-
+			}
+   }
 }
 		
