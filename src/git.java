5a0e360140d6efa9c21d5dbfd237c29914831d78
huangyingw@linux_programming <huangyingw@gmail.com>
2010-04-29 03:15:08 -0400

    n

--- src/git.java
+++ src/git.java
@@ -7,4 +7,8 @@ public class git {
 		run.exec(command);	
    }
 }
+Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+	Unhandled exception type IOException
+
+	at git.main(git.java:7)
 		
