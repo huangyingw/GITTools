362624d50353ec5a723558d6b6602060f892acaf
huangyingw@fareast <huangyingw@gmail.com>
2010-04-29 15:17:22 +0800

    n

--- src/git.java
+++ src/git.java
@@ -1,7 +1,7 @@
 import java.lang.* ;
 
 public class git {
-	public static void main(String[] args) {
+	public static void main(String[] args)throws Exception {
 		String command = "ls -al";
 		Runtime run = Runtime.getRuntime();
 		run.exec(command);	
