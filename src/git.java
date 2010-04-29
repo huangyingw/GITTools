090f9111f2f6c44dbeee613d24f4cfbaef8f8cd5
huangyingw@fareast <huangyingw@gmail.com>
2010-04-29 14:26:21 +0800

    n

--- src/git.java
+++ src/git.java
@@ -28,3 +28,12 @@ public class git {
     }
 
 }
+String cmd = "ls -al";
+		Runtime run = Runtime.getRuntime();
+		Process pr = run.exec(cmd);
+		pr.waitFor();
+		BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+		String line = "";
+		while ((line=buf.readLine())!=null) {
+			System.out.println(line);
+		}
