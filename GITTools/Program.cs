56d6fd4acc73a8ccc005c3ab424cb059fabedb4b
huangyingw@PrimaryStorage <huangyingw@gmail.com>
2010-08-14 05:03:22 -0400

    n

--- GITTools/Program.cs
+++ GITTools/Program.cs
@@ -11,55 +11,34 @@ namespace GITTools
     {
         static void Main(string[] args)
         {
-            string source = @"E:\temp\source\tools";
+            string source = @"C:\source";
             string target = @"E:\temp\target";
             DirectoryInfo sdi = new DirectoryInfo(source);
-            string result = null;
             foreach (FileSystemInfo item in sdi.GetFileSystemInfos())
             {
                 if (Directory.Exists(item.FullName))
                 {
                     if (!Directory.Exists(item.FullName + @"\.git"))
                     {
-                        result = null;
                         Console.WriteLine(item.FullName);
-                        Console.WriteLine(CallDos(item.FullName, "git in"));
-                        Console.WriteLine(CallDos(item.FullName, "git ad ."));
-                        Console.WriteLine(CallDos(item.FullName, "git ci \"Init\""));
-                    }
-                    else
-                    {
-                        string filepath = item.FullName + @"\.gitignore";
-                        if (!File.Exists(filepath))
-                        {
-                            File.Create(filepath);
-                        }
-                        using (StreamWriter sw = File.AppendText(filepath))
-                        {
-                            sw.WriteLine(string.Format("*.bak"));
-                        }
+                        Process p = new Process();
+                        p.StartInfo.FileName = "cmd.exe";
+                        p.StartInfo.UseShellExecute = false;
+                        p.StartInfo.RedirectStandardInput = true;
+                        p.StartInfo.RedirectStandardOutput = true;
+                        p.StartInfo.CreateNoWindow = true;
+                        p.Start();
+                        p.StandardInput.WriteLine("cd " + item.FullName);
+                        p.StandardInput.WriteLine("git in");
+                        p.StandardInput.WriteLine("exit");
+                        p.WaitForExit(60000);
+                        string s = p.StandardOutput.ReadToEnd();
+                        p.Close();
+                        Console.WriteLine(s);
+
                     }
                 }
             }
         }
-
-        static string CallDos(string path, string command)
-        {
-            Process p = new Process();
-            p.StartInfo.FileName = "cmd.exe";
-            p.StartInfo.UseShellExecute = false;
-            p.StartInfo.RedirectStandardInput = true;
-            p.StartInfo.RedirectStandardOutput = true;
-            p.StartInfo.CreateNoWindow = true;
-            p.Start();
-            p.StandardInput.WriteLine("cd " + path);
-            p.StandardInput.WriteLine(command);
-            p.StandardInput.WriteLine("exit");
-            p.WaitForExit(60000);
-            string s = p.StandardOutput.ReadToEnd();
-            p.Close();
-            return s;
-        }
     }
 }
-
